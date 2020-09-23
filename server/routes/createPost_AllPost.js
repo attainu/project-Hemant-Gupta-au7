107fdcbf9a31d896edcf72c3790bb1ec0e689959
Hemant kumar gupta <hemantkumargupta36@gmail.com>
2020-09-23 23:31:48 +0530

    BE_CREATEPOST_ALLPOST

---
+++ server/routes/createPost_AllPost.js
@@ -0,0 +1,54 @@
+var express = require("express");
+const mongoose = require("mongoose");
+var Post = require("../models/post");
+var requireLogin = require("../middleware/requireLogin");
+var route = express.Router();
+
+route.get("/allpost", requireLogin, (req, res) => {
+  Post.find()
+    .populate("postedBy", "_id name")
+    .populate("comments.postedBy", "_id name")
+    .then((posts) => {
+      res.status(200).json({ posts });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+route.post("/createPost", requireLogin, (req, res) => {
+  var { title, body, pic } = req.body;
+  //console.log(title,body,pic)
+  if (!title || !body || !pic) {
+    return res.status(422).json({ error: "PLEASE ADD ALL THE FIELD" });
+  }
+  var post = new Post({
+    title,
+    body,
+    photo: pic,
+    postedBy: req.user,
+  });
+  post
+    .save()
+    .then((result) => {
+      //  console.log(result)
+      res.json({ post: result });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+route.get("/mypost", requireLogin, (req, res) => {
+  Post.find({ postedBy: req.user._id })
+    .populate("postedBy", "_id name")
+    .then((mypost) => {
+      res.status(200).json({ mypost });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+
+module.exports = route;
